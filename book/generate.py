79fcbe162f68cc83a3d90eb94a416afc6b6fd49a
theta <theta.lin@yandex.com>
2022-09-24 17:02:54 +0800

    Add book generation script and book.pdf

---
+++ book/generate.py
@@ -0,0 +1,21 @@
+import glob
+import os
+
+with open('book.tex', 'w') as out:
+    out.write('\\documentclass{article}\n')
+    out.write('\\usepackage{fullpage}\n')
+    out.write('\\usepackage{minted}\n')
+    out.write('\\begin{document}\n')
+    out.write('\\tableofcontents')
+
+    file_names = glob.glob("../src/*")
+    for file_name in file_names:
+        out.write('\\section{')
+        out.write(os.path.basename(file_name).replace('_', '\\_'))
+        out.write('}\n')
+
+        out.write('\\inputminted[breaklines=true]{cpp}{')
+        out.write(file_name)
+        out.write('}\n')
+
+    out.write('\\end{document}\n')
